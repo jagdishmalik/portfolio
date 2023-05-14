168e4a9fe69c31d944a6d289733e6fefb53344be
jagdishmalik <jagdishmalik24x7@gmail.com>
2023-05-14 10:12:56 +0530

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import NavBar from './Components/NavBar';
+import Header from './Components/Header';
 function App() {
   return (
-
+   <>
       <NavBar/>
-    
+      <Header/> 
+   </>
   );
 }
 
